45b694e05bbd5215b6d7ceb403ba7d63af937064
Karan Nehra <karanehra@gmail.com>
2020-03-09 22:57:52 +0530

    Minor Change

--- main.go
+++ main.go
@@ -154,24 +154,20 @@ func setupSockServer() {
 		CheckOrigin:     func(r *http.Request) bool { return true },
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		fmt.Println(err)
+		conn, _ := upgrader.Upgrade(w, r, nil)
 
 		for {
-			// Read message from browser
 			msgType, msg, err := conn.ReadMessage()
 			if err != nil {
 				return
 			}
 
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+			fmt.Printf("%v, %s\n", msgType, string(msg))
 
-			// Write message back to browser
 			if err = conn.WriteMessage(msgType, msg); err != nil {
 				return
 			}
+
 		}
 	})
 	fmt.Println("starting sockserver")
